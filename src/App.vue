5dc9a46bfe5768901cdaf5781eb66ab645a4baef
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-09 17:00:52 +0800

    Feat: Create add new item function to form element

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <h1>{{ header }}</h1>
-    <input
-      v-model.lazy.trim="userInput"
-      type="text"
-      placeholder="35 pints of ice cream"
-    />
+    <form @submit.prevent="addToList" class="add-item-form">
+      <input
+        v-model.trim="userInput"
+        type="text"
+        placeholder="35 pints of ice cream"
+      />
+      <label>
+        <input v-model="isHighPriority" type="checkbox" />
+        High Priority
+      </label>
 
-    <label>
-      <input v-model="isHighPriority" type="checkbox" />
-      High Priority
-    </label>
+      <button type="submit" class="btn btn-primary">Save Item</button>
+    </form>
 
     <ul>
       <li v-for="{ id, item } in shoppingList" :key="id">{{ item }}</li>
@@ -29,4 +32,13 @@ const shoppingList = ref([
   { id: 2, item: "2 board games" },
   { id: 3, item: "20 cups" },
 ]);
+
+function addToList() {
+  shoppingList.value.push({
+    id: shoppingList.value.length + 1,
+    item: userInput.value,
+  });
+
+  userInput.value = "";
+}
 </script>
