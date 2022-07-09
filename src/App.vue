cb9d531fc758d119ce23a05876e4f219bf77e9c2
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-09 16:23:27 +0800

    Feat: Add shopping list

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>{{ header }}</h1>
+  <main>
+    <h1>{{ header }}</h1>
+
+    <ul>
+      <li v-for="{ id, item } in shoppingList" :key="id">{{ item }}</li>
+    </ul>
+  </main>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
 const header = ref("Shopping List App");
+const shoppingList = ref([
+  { id: 1, item: "10 party hats" },
+  { id: 2, item: "2 board games" },
+  { id: 3, item: "20 cups" },
+]);
 </script>
