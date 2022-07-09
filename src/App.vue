1e8ba8ce58b6c3c797317a2ff618ec62b022d1c8
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-09 16:44:00 +0800

    Add form for adding items

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <main>
     <h1>{{ header }}</h1>
+    <input
+      v-model.lazy.trim="userInput"
+      type="text"
+      placeholder="35 pints of ice cream"
+    />
+
+    <label>
+      <input v-model="isHighPriority" type="checkbox" />
+      High Priority
+    </label>
 
     <ul>
       <li v-for="{ id, item } in shoppingList" :key="id">{{ item }}</li>
@@ -12,6 +22,8 @@
 import { ref } from "vue";
 
 const header = ref("Shopping List App");
+const userInput = ref("");
+const isHighPriority = ref(false);
 const shoppingList = ref([
   { id: 1, item: "10 party hats" },
   { id: 2, item: "2 board games" },
